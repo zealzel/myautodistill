b6c12c47d0c6c91fe66fca0ab3eba00cdeafa357
tw13814 <zealzel@tw13814deMacBook-Pro.local>
2025-03-19 10:37:54 +0800

    add pykms.py

---
+++ flask-picamera/pykms.py
@@ -0,0 +1,9 @@
+# Dummy pykms module for picamera2
+class PixelFormat:
+    # 這裡定義一個 dummy 值，僅供 Picamera2 內部使用，不會真正使用到
+    RGB888 = "RGB888"
+    BGR888 = "BGR888"
+    XRGB8888 = "XRGB8888"
+    XBGR8888 = "XBGR8888"
+    YUV420 = "YUV420"
+    YVU420 = "YVU420"
